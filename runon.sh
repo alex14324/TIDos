f2c9190b9a06f7d103a9de3831b8a61ec8a6ff56
alex14324 <36421715+alex14324@users.noreply.github.com>
2020-04-21 15:12:42 +0530

    Add files via upload

---
+++ runon.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+#-:-:-:-:-:-:-:-:-:-:-:-:#
+#    TIDoS Framework     #
+#-:-:-:-:-:-:-:-:-:-:-:-:#
+
+#This module requires TIDoS Framework
+#https://github.com/0xInfection/TIDoS-Framework
+
+cd /opt/tidos/
+python /opt/tidos/tidos.py
